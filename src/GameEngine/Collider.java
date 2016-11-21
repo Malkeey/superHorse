0641e9d0ed054d6c44d369a1081c9e63d74a9c42
Malkeey <daniel.m.karlsson@outlook.com>
2016-11-21 05:52:09 +0100

    Collider basics done

--- src/GameEngine/Collider.java
+++ src/GameEngine/Collider.java
@@ -103,7 +103,71 @@ public class Collider extends Component {
     }
     
     private boolean xIntersect(float xMov, Collider other) {
-        
+        if(colType == ColliderType.circle && other.getColType() == ColliderType.circle) {
+            if(other != this) {
+                Transform otr = other.getTransform();
+                float xDist = Math.abs(getTransform().getX() + xMov - otr.getX());
+                float yDist = Math.abs(getTransform().getY() - otr.getY());
+                
+                double dist = Math.sqrt(xDist * xDist + yDist* yDist);
+                System.out.println("Distance: " + dist);
+                if((radius + other.getRadius()) > dist) {
+                    return true;
+                }
+            }
+            return false;
+        } else if(colType == ColliderType.rectangle && other.getColType() == CollderType.rectangle) {
+            //uy upperY, ly = lowerY, lx = leftX, rx = rightX
+            Transform t = getTransform();
+            float uy = t.getY() - height/2, ly = t.getY() + height/2;
+            float lx = t.getX() - width/2 + xMov, rx = t.getX() + width/2 + xMov;
+            
+            Transform ot = other.getTransform();
+            float ouy = ot.getY() - other.getHeight()/2, oly = ot.getY() + other.getHeight()/2;
+            float olx = ot.getX() - other.getWidth()/2, orx = ot.getX() + other.getWidth()/2;
+            
+            if(uy <= oly && uy >= ouy) { //Collided with head
+                if((rx <= orx && rx >= olx) || (lx <= orx && lx >= olx)) {
+                    return true;
+                }
+            } else if(ly <= oly && ly >= ouy) { //Collided with feet
+                if ((rx <= orx && rx >= olx) || (lx <= orx && lx >= olx)) {
+                    if(physics != null) {
+                        physics.setGrounded(true);
+                    }
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public final Transform getTransform() {
+        return getOwnerTransform();
+    }
+    
+    public double getRadius() {
+        return radius;
+    }
+    
+    public ColliderType getColType() {
+        return colType;
+    }
+    
+    public int getHeight() {
+        return height;
+    }
+    
+    public int getWidth() {
+        return width;
+    }
+    
+    @Override
+    public void setOwner(GameObject owner) {
+        this.owner = owner;
+        if(owner.hasComponent(Constants.ComponentType.physics)) {
+            physics = (Physics)owner.getComponent(Constants.ComponentType.physics);
+        }
     }
 
 }
