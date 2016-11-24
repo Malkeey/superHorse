9017f78a09c57786b895907dbb6afa27f03f2029
Malkeey <daniel.m.karlsson@outlook.com>
2016-11-24 21:02:43 +0100

    Constants basics done

--- src/GameEngine/Constants.java
+++ src/GameEngine/Constants.java
@@ -6,8 +6,20 @@
 package GameEngine;
 
 
-public class Constants {
+public final class Constants {
 
-    // TODO code application logic here
+    //Holds constants for the game
+    
+    //Holds references to all sub-systems and game engine
+    
+    public static enum ComponentType {graphic, physics, sound, controller, collider};
+    public static enum Event {collision};
+    
+    public static GameEngine theGameEngine = null;
+    public static InputHandler theInputHandler = null;
+    public static Loader theLoader = null;
+    public static PhysicsEngine thePhysicsEngine = null;
+    public static Renderer theRenderer = null;
+    public static SoundEngine theSoundEngine = null;
 
 }
