1e2c70afa4b3283d9a83e49a901dfbb57ffec1ed
Malkeey <daniel.m.karlsson@outlook.com>
2016-11-25 10:03:24 +0100

    Sound basics done

--- src/GameEngine/Sound.java
+++ src/GameEngine/Sound.java
@@ -5,9 +5,31 @@
  */
 package GameEngine;
 
+import java.io.File;
+import java.util.HashMap;
 
-public class Sound {
+import javax.sound.sampled.Clip;
 
-    // TODO code application logic here
+import game_engine.Constants.Event;
 
+public class Sound extends Component {
+
+    //Sound related data, such as what file to play and at what volume
+    //Possibly playback speed etc
+    
+    double volume = 1;
+    HashMap<Event, File> eventClips = new HashMap<Event, File>();
+    
+    public Sound(String path, Event event) {
+        compType = Constants.ComponentType.sound;
+        File temp = Constants.theLoader.loadSound(path);
+        if(temp != null) {
+            eventClips.put(event, temp);
+        }
+    }
+    
+    public File getEventSound(Event event) {
+        return eventClips.get(event);
+    }
+    
 }
