0cbdf0f5b5a82edadb4343d74337e25bd2dda25a
Malkeey <daniel.m.karlsson@outlook.com>
2016-11-25 09:56:34 +0100

    Graphic basics done

--- src/GameEngine/Graphic.java
+++ src/GameEngine/Graphic.java
@@ -5,9 +5,35 @@
  */
 package GameEngine;
 
+import java.awt.image.BufferedImage;
 
-public class Graphic {
+public class Graphic extends Component {
 
-    // TODO code application logic here
+    //Contains graphical data based on information in the component class
+    
+    private BufferedImage sprite;
+    private in height, width;
+    
+    public Graphic(String imagePath, int height, int width) {
+        compType = Constants.ComponentType.graphic;
+        this.height = height;
+        this.width = width;
+        
+        if(imagePath != null) {
+            sprite = Constants.theLoader.loadImage(imagePath);
+        }
+    }
+    
+    public BufferedImage getSprite() {
+        return sprite;
+    }
+    
+    public int getHeight() {
+        return height;
+    }
+    
+    public int getWidth() {
+        return width;
+    }
 
 }
