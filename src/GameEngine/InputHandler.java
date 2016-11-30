261265334603502d967ad2625e31ac2e29f95cfd
Daniel Karlsson <daniel.m.karlsson@outlook.com>
2016-11-30 01:57:59 +0100

    InputHandler Basice Done

--- src/GameEngine/InputHandler.java
+++ src/GameEngine/InputHandler.java
@@ -5,9 +5,50 @@
  */
 package GameEngine;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
-public class InputHandler {
+public class InputHandler implements KeyListener {
 
-    // TODO code application logic here
+    // Handles inputs, keyboard mapping etc to allow the player to control the game
+    
+    private ArrayList<GameObjectController> controllers = new ArrayList<GameObjectController>();
+    
+    private InputHandler() {
+        
+    }
+    
+    public static InputHandler getInstance() {
+        if(Constants.theInputHandler == null) {
+            Constants.theInputHandler = new InputHandler();
+        }
+        return Constants.theInputHandler;
+    }
+    
+    public void addController(GameObjectController newController) {
+        controllers.add(newController);
+    }
+    
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
+    }
+    
+    @Override
+    public void keyPressed(KeyEvent e) {
+        Integer key = e.getKeyCode();
+        for(GameObjectController controller : controllers) {
+            controller.addInput(key);
+        }
+    }
+    
+    @Override
+    public void keyReleased(KeyEvent e) {
+        Integer key = e.getKeyCode();
+        for(GameObjectController controller : controllers) {
+            controller.removeInput(key);
+        }
+    }
 
 }
