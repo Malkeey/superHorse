be67fea45ddba0d7dd05a84e6121a8bd2a24b3a3
Daniel Karlsson <daniel.m.karlsson@outlook.com>
2016-11-30 02:16:56 +0100

    Renderer Basics Done

--- src/GameEngine/Renderer.java
+++ src/GameEngine/Renderer.java
@@ -5,9 +5,51 @@
  */
 package GameEngine;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.util.ArrayList;
 
-public class Renderer {
+import javax.swing.JPanel;
+//import java.awt.event.*;
 
-    // TODO code application logic here
+public class Renderer extends JPanel {
+
+    // Renders graphical components to the screen
+    // For later stages: decide what components to render to avoid rendering assets that can't be seen
+    
+    private static final long serialVersionUID = 7904606405657475749L;
+    private ArrayList<Graphic> graphicComponents = new ArrayList<Graphic>();
+    
+    private Renderer() {
+        
+    }
+    
+    public static Renderer getInstance() {
+        if(Constants.theRenderer == null) {
+            Constants.theRenderer = new Renderer();
+        }
+        return Constants.theRenderer;
+    }
+    
+    public void addGraphicComponent(Graphic g) {
+        graphicComponents.add(g);
+    }
+    
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for(Graphic graphic : graphicComponents) {
+            Image sprite = graphic.getSprite();
+            Transform temp = graphic.getOwnerTransform();
+            if(sprite != null) {
+                //g.drawImage(img, dx1, dy1, dx2, dy2, sx1, sy1, sx2, sy2, observer)
+                g.drawImage(sprite, (int)temp.getX(), (int)temp.getY(), null);
+            } else {
+                g.setColor(Color.red);
+                g.fillRect((int)temp.getX() - graphic.getWidth()/2, (int)temp.getY() + graphic.getHeight()/2, graphic.getWidth(), graphic.getHeight());
+            }
+        }
+    }
 
 }
