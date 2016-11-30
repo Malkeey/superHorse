a19640ec428b90d7c1e405d17a0b44a336bdc7d4
Daniel Karlsson <daniel.m.karlsson@outlook.com>
2016-11-30 02:49:28 +0100

    GameObjectController Basics done

--- src/GameEngine/GameObjectController.java
+++ src/GameEngine/GameObjectController.java
@@ -5,9 +5,56 @@
  */
 package GameEngine;
 
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
-public class GameObjectController {
+public class GameObjectController extends Component {
 
-    // TODO code application logic here
+    private int up = KeyEvent.VK_UP, right = KeyEvent.VK_RIGHT, down = KeyEvent.VK_DOWN, left = KeyEvent.VK_LEFT; //keycodes
+    private ArrayList<Integer> input = new ArrayList<Integer>();
+    private Physics physicsComponent;
+    
+    public GameObjectController(Physics physicsComponent) {
+        compType = Constants.ComponentType.controller;
+        this.physicsComponent = physicsComponent;
+    }
+    
+    public GameObjectController(Physics physicsComponent, int keyCodeUp, int keyCodeRight, int keyCodeDown, int keyCodeLeft) {
+        compType = Constants.ComponentType.controller;
+        this.physicsComponent = physicsComponent;
+        up = keyCodeUp;
+        right = keyCodeRight;
+        down = keyCodeDown;
+        left = keyCodeLeft;
+    }
+    
+    public void tick(float delta) {
+        if(!input.isEmpty()) {
+            if(input.contains(up)) {
+                physicsComponent.moveUp();
+            }
+            if(input.contains(right)) {
+                physicsComponent.moveRight();
+            }
+            if(input.contains(down)) {
+                physicsComponent.moveDown();
+            }
+            if(input.contains(left)) {
+                physicsComponent.moveLeft();
+            }
+        }
+    }
+    
+    public void addInput(Integer pressedKey) {
+        if(!input.contains(pressedKey)) {
+            input.add(pressedKey);
+        }
+    }
+    
+    public void removeInput(Integer pressedKey) {
+        if(input.contains(pressedKey)) {
+            input.remove(pressedKey);
+        }
+    }
 
 }
