dee4baee2f88a41cbc6348155872996d8c5de63f
Daniel Karlsson <daniel.m.karlsson@outlook.com>
2016-11-30 01:36:49 +0100

    Transform basics done

--- src/GameEngine/Transform.java
+++ src/GameEngine/Transform.java
@@ -6,8 +6,44 @@
 package GameEngine;
 
 
-public class Transform {
+public class Transform extends Component {
 
-    // TODO code application logic here
+    private float x, y;
+    private float scale;
+    
+    public Transform(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public Transform(int x, int y, float scale) {
+        this.x = x;
+        this.y = y;
+        this.scale = scale;
+    }
+    
+    public float getX() {
+        return x;
+    }
+    
+    public float getY() {
+        return y;
+    }
+    
+    public float getScale() {
+        return scale;
+    }
+    
+    public void setX(float x) {
+        this.x = x;
+    }
+    
+    public void setY(float y) {
+        this.y = y;
+    }
+    
+    public void setScale(float scale) {
+        this.scale = scale;
+    }
 
 }
